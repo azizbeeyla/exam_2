1c24345782b899e33a9e8a29685dcef59c727e30
azizbeeyla <azizbeeyla@gmail.com>
2025-08-08 17:43:34 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(MyExamApp());
+}
+class MyExamApp extends StatelessWidget {
+  const MyExamApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      home: ,
+    );
+  }
+}
